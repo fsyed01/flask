3c97d21c05deea000aca7b7a8bedff28569ee6c0
Fairoz__Syed <fairozrehman.syed@slu.edu>
2023-12-02 17:28:12 -0600

    second commit

--- app.py
+++ app.py
@@ -32,3 +32,16 @@ def get_tweet_by_id(tweet_id):
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
+
+# Example curl request for the endpoint that returns hello world
+# curl http://localhost:5001/
+## Expected response: Hello world
+# Example curl request for the endpoint that returns all tweets
+# curl http://localhost:5000/tweets
+# Expected response: JSON data of all tweets
+# Example curl request for the endpoint that returns a specific tweet by ID
+# curl http://localhost:5000/tweet/123
+# Expected response: JSON data of the tweet with ID 123
+# Example curl request for the endpoint that adds a new tweet
+# curl -X POST -H "Content-Type: application/json" -d '{"text": "New tweet"}' http://localhost:5000/tweet
+# Expected response: JSON data of the newly added tweet
